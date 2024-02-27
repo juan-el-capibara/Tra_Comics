e7df7502939b69100456f3e0eb2d980d9722c797
Kelilopz <kelilopz2171804@gmail.com>
2024-02-27 09:01:34 -0500

    Update

--- app.js
+++ app.js
@@ -36,41 +36,169 @@ const personajes =[
 
     {"id": "5d86371f97c29d020f1e1f6d",
     "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
-    "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/48/Falcon_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212822",
-"squarePic": "https://www.tooys.mx/pub/media/catalog/product/cache/89d4a95274a31fe8bdfcc437494b2c9e/f/a/falcon_marvel_gallery_5c4dc5df6c14d_-_copia.jpg",
+    "picture": "https://i.pinimg.com/736x/5d/5f/b3/5d5fb3838cb504b9eb4ab0f0b068d0b5.jpg",
+"squarePic": "https://cdn.marvel.com/u/prod/marvel/i/mg/c/80/6048fddcc96b0/clean.jpg",
     "name": "Falcon"}
 ,
 
     {"id": "5d86371fd55e2e2a30fe1ccb",
     "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-    "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/99/Black_Panther_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212436",
-"squarePic": "http://www.mundocinematografico.net/wp-content/uploads/2018/02/black-panther-marvel-cosas-felices.jpg",
+    "picture": "https://hips.hearstapps.com/hmg-prod/images/panther-and-namor-1668195702.jpeg",
+"squarePic": "https://img.europapress.es/fotoweb/fotonoticia_20180215100212_1200.jpg",
     "name": "Black Panther"}
 ,
 
     {"id": "5d86371fd55e2e2a30fe1ccb1",
     "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-    "picture": "https://www.outland.no/media/catalog/product/cache/6f3b753be090e58846a92333dfe1de97/4/8/4897011186276_4.jpg",
-"squarePic": "https://img-cdn.hipertextual.com/files/2019/09/hipertextual-filtracion-revela-que-doctor-strange-2-resucitara-dos-personajes-muertos-marvel-2019435911.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1",
+    "picture": "https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/THZDVQPJKZBOTASZX7F7JKPA4M.jpg",
+"squarePic": "https://cdn.mos.cms.futurecdn.net/y2dDqaZS6grPVZqSN9c8j4.jpg",
     "name": "Doctor Strange"}
 , 
     {"id": "5d86371fd55e2e2a30fe1ccb2",
     "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-    "picture": "https://www.sideshow.com/storage/product-images/903735/spider-man-advanced-suit_marvel_silo.png",
-"squarePic": "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/09/28/15696592030335.jpg",
+    "picture": "https://i.ebayimg.com/images/g/aJAAAOSwdR5kfkgM/s-l1600.jpg",
+"squarePic": "https://img2.rtve.es/i/?w=1600&i=1442912664626.jpg",
     "name": "Spider Man"}
 ,
 
     {"id": "5d86371fd55e2e2a30fe1cc3",
     "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-    "picture": "https://fsmedia.imgix.net/f5/ba/d8/66/7dd4/44e9/8e0a/d91348489582/ant-man-and-the-wasp-2018.jpeg#image",
-"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "picture": "https://media.gq.com.mx/photos/5f612d5ee568fa5953a7764d/master/w_1600%2Cc_limit/ant-man%2520astonishing.jpg",
+"squarePic": "https://www.hollywoodreporter.com/wp-content/uploads/2019/11/ant-man_cover-publicity-h_2019.jpg",
     "name": "Ant Man"}
 ,
 
     {"id": "5d86371fd55e2e2a30fe1cc4",
     "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-    "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
-"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "picture": "https://comicbookdispatch.com/wp-content/uploads/2023/03/wasp2023003_preview_page_1.jpg",
+"squarePic": "https://cdn.marvel.com/u/prod/marvel/i/mg/a/c0/5498725102772/clean.jpg",
     "name": "Wasp"}
+,
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://www.shopknuckleheads.com/cdn/shop/products/29920MV_500x700.jpg?v=1648744917",
+"squarePic": "https://images.comicbooktreasury.com/wp-content/uploads/2022/07/Thor-Reading-Order-1.jpg",
+    "name": "Thor"}
+,
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://cdn.marvel.com/u/prod/marvel/i/mg/3/00/63613a5218ce3/clean.jpg",
+"squarePic": "https://www.superherohype.com/wp-content/uploads/sites/4/2023/12/Deadpool-2024.jpg?w=1024",
+    "name": "Dead Pool"}
+,
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://m.media-amazon.com/images/I/81wwCPO8uTS.jpg",
+"squarePic": "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/06/Estos-son-los-comics-que-inspiraron-la-nueva-obra-de-Marvel-Loki-1.png?fit=1280%2C720&quality=80&ssl=1",
+    "name": "Loki"}
+
+
+
+
+,{"id": "5d86371f1efebc31def272e2",
+    "about": "Ipsum duis incididunt ullamco tempor. Amet incididunt Lorem consequat labore culpa. Pariatur amet veniam reprehenderit sunt laborum excepteur. Labore eu ut ut Lorem labore aliqua quis ex elit nulla in incididunt commodo aliquip. Velit excepteur eiusmod Lorem esse officia. Irure aliquip Lorem fugiat voluptate dolor duis consectetur aliquip pariatur tempor reprehenderit deserunt.",
+    "picture": "https://static.wikia.nocookie.net/marvel_dc/images/0/08/Batman_Vol_3_131_Textless_Fabok_Variant.jpg/revision/latest?cb=20230106222502",
+"squarePic": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/08/batman-comics-dc-2791981.jpg?tf=1200x900",
+    "name":" Batman"}
+,
+
+    {"id": "5d86371f2343e37870b91ef1",
+    "about": "Mollit officia ad excepteur anim proident incididunt eiusmod mollit consectetur id sit velit. Laborum ut magna officia qui laboris eiusmod do culpa. Culpa dolor officia velit cillum culpa deserunt cupidatat cillum ipsum laborum.",
+    "picture": "https://www.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2016/03/571004-superman-mejores-comics-hombre-acero.jpg",
+"squarePic": "https://images.comicbooktreasury.com/wp-content/uploads/2022/07/Superman-Modern-Age-Reading-Order.jpg",
+    "name": "Superman"}
+,
+
+    {"id": "5d86371f25a058e5b1c8a65e",
+    "about": "Laboris est duis eiusmod adipisicing cillum ut sit ea Lorem non laboris quis Lorem. Est culpa esse aliqua non labore dolor esse labore nulla mollit. Nostrud amet est quis adipisicing dolor. Labore veniam elit veniam non ad ex consequat excepteur eiusmod. Minim cupidatat velit Lorem culpa quis consequat incididunt qui amet incididunt pariatur ex aliquip aliqua. Magna ex elit in aliquip minim eu ut ut fugiat ullamco deserunt adipisicing cillum.",
+    "picture": "https://i.pinimg.com/originals/30/0a/5a/300a5a877ad58601b2b4ad322757455b.jpg",
+"squarePic": "https://e.rpp-noticias.io/xlarge/2020/10/22/584158_1013280.jpg",
+    "name": "La mujer maravilla"}
+,
+
+    {"id": "flash",
+    "about": "Ipsum tempor sunt Lorem est. Fugiat nisi velit veniam labore et ullamco minim adipisicing do culpa. Cillum voluptate reprehenderit aute consectetur.",
+    "picture": "https://static.wikia.nocookie.net/comicdc/images/f/f3/The_Flash_Vol_4_1.jpg/revision/latest?cb=20130804180654&path-prefix=es",
+"squarePic": "https://s3.amazonaws.com/dam.smashmexico.com.mx/wp-content/uploads/2021/09/origen-del-multiverso-dc-flash-123-cover.jpg",
+    "name": "Flash"}
+,
+
+    {"id": "aquaman",
+    "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
+    "picture": "https://cdn2.penguin.com.au/covers/original/9781779510198.jpg",
+"squarePic": "https://i.blogs.es/fc993d/aquaman-new-52-comics/1366_2000.jpg",
+    "name": "Aquaman"}
+,
+
+    {"id": "eljoker",
+    "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
+    "picture": "https://content.eccediciones.com/productos/8042/PORTADA_JPG_WEB_joker_asylum.jpg",
+"squarePic": "https://i.blogs.es/3d011b/joker-comics/1366_2000.jpeg",
+    "name": "El joker"}
+,
+
+    {"id": "5d86371fd55e2e2a30fe1ccb",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://content.eccediciones.com/productos/3043/Nightwing_1.jpg",
+"squarePic": "https://cdn.vox-cdn.com/thumbor/H-CNRXkPTiPRN1XrWD9GHj_3pho=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22855157/NTW_Cv87.jpg",
+    "name": "Dick Grayson"}
+,
+
+    {"id": "5d86371fd55e2e2a30fe1ccb1",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://m.media-amazon.com/images/I/A1Ra+mgu-ZL._AC_UF1000,1000_QL80_.jpg",
+"squarePic": "https://img.europapress.es/fotoweb/fotonoticia_20181029172611_1200.jpg",
+    "name": "Bane"}
+, 
+    {"id": "5d86371fd55e2e2a30fe1ccb2",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://content.eccediciones.com/productos/18217/cubierta_hellblazer_vol1_pecados_originales_WEB.jpg",
+"squarePic": "https://i.ytimg.com/vi/VikIxWtLqhk/maxresdefault.jpg",
+    "name": "Jhon Constantine"}
+,
+
+    {"id": "5d86371fd55e2e2a30fe1cc3",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://comicnewbies.com/wp-content/uploads/2020/10/Alfred-Pennyworth-Batman-Vol.-3-97.jpg",
+"squarePic": "https://www.mundodeportivo.com/alfabeta/hero/2023/04/dsfaas.png?width=1200",
+    "name": "Al fred"}
+,
+
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://i.pinimg.com/736x/56/e1/36/56e1365ba542d684ae1a0e9d3839f446.jpg",
+"squarePic": "https://64.media.tumblr.com/b5e9f3092be6337c9bd24a64219cf310/a76a924b03553d8c-3f/s1280x1920/214edabd7c1e4efe97cca01a043346cfff6121b2.jpg",
+    "name": "Hal Jordan"}
+,
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://hips.hearstapps.com/hmg-prod/images/martian-manhunter-2-fotogramas-1616102765.jpg",
+"squarePic": "https://www.yocomics.com/wp-content/uploads/1605209526_Martian-Manhunter-JLA.jpg",
+    "name": "Detective Marciano"}
+,
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://comiqueros.cl/wp-content/uploads/2021/10/RCO001.jpg",
+"squarePic": "https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2021/03/12234413/Fate-header.jpg",
+    "name": "Dr Fate"}
+,
+    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://www.eslahoradelastortas.com/blog/media/2022/04/GA-80-1.jpg",
+"squarePic": "https://i0.wp.com/www.tomosygrapas.com/wp-content/uploads/2021/03/Green-Arrow-celebra-su-80-Cumplean%CC%83os-con-un-especial-de-100-pa%CC%81ginas.jpeg?resize=747%2C487&ssl=1",
+    "name": "Flecha Verde"}
+
+,    {"id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEif-vgFf3DsTpP0MHE2EmSe6cxqX-__RyBxYdML-PDf3D8U7--vqGkxUvvl-Svl7KnLT0Ul8zULFqWZ5Gy0gPTp1MRKWIhHukekHpkDAVI9bwYTMlgi-SuPo_8q8TOSzEnjSdBOeB1RYBCE_qI78DdU4o1kz_SwiAN1FiBH8VITNbITB1XWyP9q1LkG1A/s3056/Cyborg-1-1.jpg",
+"squarePic": "https://www.comicbookherald.com/wp-content/uploads/2020/03/cyborg-1-scaled.jpg",
+    "name": "Cyborg"}
+
 ]
+
+
+window.onload = hero1 
+function hero1() {    
+     let spiderman = document.getElementById('spiderman')     
+     let galery = personajes[0].picture     
+     spiderman.src= galery}
